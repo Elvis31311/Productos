4856edf85e4ba37725231ed90e15324e21840fb4
Elvis Sánchez <elvis.sanchez@cenestur.edu.ec>
2026-03-03 20:00:43 -0500

    refactor: remove placeholder comment in conection.ts

--- src/backend/Conection/conection.ts
+++ src/backend/Conection/conection.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Sustituye con tus datos reales de la pantalla de Supabase
 const supabaseUrl = 'https://omhjwgusqwodicnqetpl.supabase.co' 
 const supabaseAnonKey = 'sb_publishable_dKWylgDonrzcp6CVKSexOw_R2OcM_dR'
 
